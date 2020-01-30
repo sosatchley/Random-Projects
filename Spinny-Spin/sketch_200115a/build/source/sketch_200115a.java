16e92e4ca7c1d69b5dde9ce697a94e4a4f2a4029
Shane <atchley@hendrix.edu>
2020-01-29 18:05:43 -0600

    Abstracting
    Moved code to separate functions

--- Spinny-Spin/sketch_200115a/build/source/sketch_200115a.java
+++ Spinny-Spin/sketch_200115a/build/source/sketch_200115a.java
@@ -20,9 +20,10 @@ public class sketch_200115a extends PApplet {
 
 
 int state;
-int i;
-float[] x = new float[10];
-int[] fills = new int[10];
+int currentArc;
+int numberOfArcs = 10;
+float[] arcMeasures = new float[numberOfArcs];
+float[] arcRadii = new float[numberOfArcs];
 float rotation;
 float gradient;
 float curColor;
@@ -40,8 +41,9 @@ public void setup() {
     colorMode(HSB, 100, 100, 100);
 
     state = 0;
-    i = 0;
-    Arrays.fill(x,0);
+    int currentArc = 0;
+    Arrays.fill(arcMeasures, 0);
+    Arrays.fill(arcRadii, 0);
     rotation = 0;
     gradient = 0.0001f;
     lastColor = 100;
@@ -61,86 +63,74 @@ public void draw() {
     background(0);
 
     if (state == 0) {
-        for (int j = 0; j < i; j++) {
-            fill(color(lastColor, 0, 100));
-            arc(0,0, 180 ,360 - (j * 30),10);
-        }
-
-        if (x[i] > 179.5f) {
-            if (i < 9) {
-                i++;
-            } else {
-                curColor = PApplet.parseInt(random(100));
-                lastColor = 0;
-                state = 1;
-            }
-        } else {
-            x[i] = lerp(x[i],181,.1f);
-        }
-
-        float diam = ((2*x[i]) - (i * 30));
-        if (diam < 0) {
-            noFill();
-        } else {
-            fill(color(lastColor, 0, 100));
-        }
-        arc(0,0, x[i] , diam,10);
+        fill(color(100, 0, 100));
+        growArcs(0, 0, 10, 180, 360, 30, 10);
     }
 
     if (state == 1) {
-        gradient = lerp(gradient, 0.05f, 0.005f);
-        rotation = lerp(rotation, radians(-360), gradient);
-        if ((rotation*-1) < radians(180)) {
-            lastColor = lerp(lastColor, 100, gradient*2);
-        } else {
-            lastColor = lerp(lastColor, 0, gradient/2);
-        }
-        println(lastColor);
-        // color curColor = style[int(random(10))];
-
-        for (int j = 0; j <= i; j++) {
-            fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
-            pushMatrix();
-            float r = rotation*(10.0f-j);
-            rotate(r);
-            arc(0,0, 270 ,360 - (j * 30),10);
-            popMatrix();
-        }
-        if ((rotation*(-1) + gradient*0.01f) >= radians(360)) {
-            state = 2;
-            gradient = 0.0001f;
-            rotation = 0;
-            curColor = PApplet.parseInt(random(100));
-        }
+        whirlyDirly(-1, false);
     }
 
     if (state == 2) {
-        gradient = lerp(gradient, 0.05f, 0.005f);
-        rotation = lerp(rotation, radians(360), gradient);
-        if (rotation < radians(180)) {
-            lastColor = lerp(lastColor, 100, gradient*2);
-        } else {
-            lastColor = lerp(lastColor, 0, gradient/2);
-        }
-        curColor = PApplet.parseInt(random(100));
-
-        for (int j = 0; j <= i; j++) {
-            fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
-            pushMatrix();
-            float r = rotation*(10.0f-j);
-            rotate(r);
-            arc(0,0, 180 ,360 - (j * 30),10);
-            popMatrix();
-        }
-        if ((rotation + gradient*.01f) >= radians(360)) {
-            state = 1;
-            gradient = 0.0001f;
-            rotation = 0;
-            curColor = PApplet.parseInt(random(100));
-        }
+        whirlyDirly(1, true);
     }
 }
 
+public void growArcs(float x, float y, int numberOfArcs,
+              float degrees, float radius, float spacing, float thickness) {
+
+    float threshold = 1.0f;
+
+    for (int i = 0; i < currentArc; i++) {
+        // fill(color(hue, 0, 100));
+        arc(x,y, degrees ,radius - (i * spacing),thickness);
+    }
+
+    if (arcMeasures[currentArc] > degrees - threshold &&
+        arcRadii[currentArc] > (radius - (currentArc * spacing))-threshold) {
+        if (currentArc >= numberOfArcs - 1) {
+            curColor = PApplet.parseInt(random(100));
+            lastColor = 0;
+            state = 1;
+        } else {
+            currentArc++;
+            println(currentArc);
+        }
+    } else {
+        arcMeasures[currentArc] = lerp(arcMeasures[currentArc], degrees + threshold, .1f);
+        arcRadii[currentArc] = lerp(arcRadii[currentArc], (radius - (currentArc
+                                * spacing)) + threshold, 0.1f);
+    }
+    if (arcRadii[currentArc] < 0) {noFill();}
+    arc(x,y, arcMeasures[currentArc] , arcRadii[currentArc], thickness);
+}
+
+public void whirlyDirly(int dir, boolean strobe) {
+    gradient = lerp(gradient, 0.05f, 0.005f);
+    rotation = lerp(rotation, radians(360 * dir), gradient);
+    curColor = (strobe) ? PApplet.parseInt(random(100)) : curColor;
+
+    if (Math.abs(rotation) < radians(180)) {
+        lastColor = lerp(lastColor, 100, gradient*2);
+    } else {
+        lastColor = lerp(lastColor, 0, gradient/2);
+    }
+
+    for (int j = 0; j <= currentArc; j++) {
+        fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
+        pushMatrix();
+        float r = rotation*(10.0f-j);
+        rotate(r);
+        arc(0,0, 180 ,360 - (j * 30),10);
+        popMatrix();
+    }
+    if (Math.abs(rotation) + gradient*.01f >= radians(360)) {
+        gradient = 0.0001f;
+        rotation = 0;
+        curColor = PApplet.parseInt(random(100));
+        state = (state == 1) ? 2 : 1;
+    }
+}
 // Draw solid arc
 public void arc(float x,float y,float deg,float rad,float w) {
   int a = PApplet.parseInt(min (deg/SINCOS_PRECISION,SINCOS_LENGTH-1));
--- Spinny-Spin/sketch_200115a/sketch_200115a.pde
+++ Spinny-Spin/sketch_200115a/sketch_200115a.pde
@@ -2,9 +2,10 @@ import java.util.Arrays;
 
 
 int state;
-int i;
-float[] x = new float[10];
-color[] fills = new color[10];
+int currentArc;
+int numberOfArcs = 10;
+float[] arcMeasures = new float[numberOfArcs];
+float[] arcRadii = new float[numberOfArcs];
 float rotation;
 float gradient;
 float curColor;
@@ -22,8 +23,9 @@ void setup() {
     colorMode(HSB, 100, 100, 100);
 
     state = 0;
-    i = 0;
-    Arrays.fill(x,0);
+    int currentArc = 0;
+    Arrays.fill(arcMeasures, 0);
+    Arrays.fill(arcRadii, 0);
     rotation = 0;
     gradient = 0.0001;
     lastColor = 100;
@@ -43,86 +45,74 @@ void draw() {
     background(0);
 
     if (state == 0) {
-        for (int j = 0; j < i; j++) {
-            fill(color(lastColor, 0, 100));
-            arc(0,0, 180 ,360 - (j * 30),10);
-        }
-
-        if (x[i] > 179.5) {
-            if (i < 9) {
-                i++;
-            } else {
-                curColor = int(random(100));
-                lastColor = 0;
-                state = 1;
-            }
-        } else {
-            x[i] = lerp(x[i],181,.1);
-        }
-
-        float diam = ((2*x[i]) - (i * 30));
-        if (diam < 0) {
-            noFill();
-        } else {
-            fill(color(lastColor, 0, 100));
-        }
-        arc(0,0, x[i] , diam,10);
+        fill(color(100, 0, 100));
+        growArcs(0, 0, 10, 180, 360, 30, 10);
     }
 
     if (state == 1) {
-        gradient = lerp(gradient, 0.05, 0.005);
-        rotation = lerp(rotation, radians(-360), gradient);
-        if ((rotation*-1) < radians(180)) {
-            lastColor = lerp(lastColor, 100, gradient*2);
-        } else {
-            lastColor = lerp(lastColor, 0, gradient/2);
-        }
-        println(lastColor);
-        // color curColor = style[int(random(10))];
-
-        for (int j = 0; j <= i; j++) {
-            fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
-            pushMatrix();
-            float r = rotation*(10.0-j);
-            rotate(r);
-            arc(0,0, 180 ,360 - (j * 30),10);
-            popMatrix();
-        }
-        if ((rotation*(-1) + gradient*0.01) >= radians(360)) {
-            state = 2;
-            gradient = 0.0001;
-            rotation = 0;
-            curColor = int(random(100));
-        }
+        whirlyDirly(-1, false);
     }
 
     if (state == 2) {
-        gradient = lerp(gradient, 0.05, 0.005);
-        rotation = lerp(rotation, radians(360), gradient);
-        if (rotation < radians(180)) {
-            lastColor = lerp(lastColor, 100, gradient*2);
-        } else {
-            lastColor = lerp(lastColor, 0, gradient/2);
-        }
-        curColor = int(random(100));
-
-        for (int j = 0; j <= i; j++) {
-            fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
-            pushMatrix();
-            float r = rotation*(10.0-j);
-            rotate(r);
-            arc(0,0, 180 ,360 - (j * 30),10);
-            popMatrix();
-        }
-        if ((rotation + gradient*.01) >= radians(360)) {
-            state = 1;
-            gradient = 0.0001;
-            rotation = 0;
-            curColor = int(random(100));
-        }
+        whirlyDirly(1, true);
     }
 }
 
+void growArcs(float x, float y, int numberOfArcs,
+              float degrees, float radius, float spacing, float thickness) {
+
+    float threshold = 1.0;
+
+    for (int i = 0; i < currentArc; i++) {
+        // fill(color(hue, 0, 100));
+        arc(x,y, degrees ,radius - (i * spacing),thickness);
+    }
+
+    if (arcMeasures[currentArc] > degrees - threshold &&
+        arcRadii[currentArc] > (radius - (currentArc * spacing))-threshold) {
+        if (currentArc >= numberOfArcs - 1) {
+            curColor = int(random(100));
+            lastColor = 0;
+            state = 1;
+        } else {
+            currentArc++;
+            println(currentArc);
+        }
+    } else {
+        arcMeasures[currentArc] = lerp(arcMeasures[currentArc], degrees + threshold, .1);
+        arcRadii[currentArc] = lerp(arcRadii[currentArc], (radius - (currentArc
+                                * spacing)) + threshold, 0.1);
+    }
+    if (arcRadii[currentArc] < 0) {noFill();}
+    arc(x,y, arcMeasures[currentArc] , arcRadii[currentArc], thickness);
+}
+
+void whirlyDirly(int dir, boolean strobe) {
+    gradient = lerp(gradient, 0.05, 0.005);
+    rotation = lerp(rotation, radians(360 * dir), gradient);
+    curColor = (strobe) ? int(random(100)) : curColor;
+
+    if (Math.abs(rotation) < radians(180)) {
+        lastColor = lerp(lastColor, 100, gradient*2);
+    } else {
+        lastColor = lerp(lastColor, 0, gradient/2);
+    }
+
+    for (int j = 0; j <= currentArc; j++) {
+        fill(color(curColor, lastColor, 100-(j*(lastColor/10))));
+        pushMatrix();
+        float r = rotation*(10.0-j);
+        rotate(r);
+        arc(0,0, 180 ,360 - (j * 30),10);
+        popMatrix();
+    }
+    if (Math.abs(rotation) + gradient*.01 >= radians(360)) {
+        gradient = 0.0001;
+        rotation = 0;
+        curColor = int(random(100));
+        state = (state == 1) ? 2 : 1;
+    }
+}
 // Draw solid arc
 void arc(float x,float y,float deg,float rad,float w) {
   int a = int(min (deg/SINCOS_PRECISION,SINCOS_LENGTH-1));
